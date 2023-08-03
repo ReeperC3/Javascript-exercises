a1b97379a208619223af633db6badce242ddcaa7
ReeperC3 <cian3neiland@gmail.com>
2023-08-03 20:44:05 +0100

    Completed exercises 1 through 7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let num = 0;
+  if (start < 0 || !Number.isInteger(start) || !Number.isInteger(end) || end < 0) {
+    return "ERROR"
+  } 
+  if (end < start) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  
+  for (let i = start; i <= end; i++) {
+    num += i;
+  }
+  
+  return num;
+  
 };
 
 // Do not edit below this line
